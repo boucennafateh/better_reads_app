a3af015d3b7b04ccfc46c8728ff08ed8249cec6d
Fateh Boucenna <boucenna.fateh@gmail.com>
2021-11-01 23:47:07 +0100

    setting up book search

--- src/main/java/io/javabrains/search/SearchController.java
+++ src/main/java/io/javabrains/search/SearchController.java
@@ -1,15 +1,28 @@
 package io.javabrains.search;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Controller
 public class SearchController {
 
-    @RequestMapping("/search")
-    public String getSearch(@RequestParam String query){
+    private final WebClient webClient;
 
+    public SearchController(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder
+                .baseUrl("http://openlibrary.org/search.json").build();
+    }
+
+    @GetMapping("/search")
+    public String getSearch(@RequestParam String query, Model model){
+        SearchResult result = webClient.get()
+                .uri("?q={query}", query)
+                .retrieve().bodyToMono(SearchResult.class)
+                .block();
+        model.addAttribute("searchResult", result);
         return "search";
     }
 }
